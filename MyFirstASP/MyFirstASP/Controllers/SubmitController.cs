210cca9b5ced6957d9d54c8d75590e1e8ce7fcfd
Stanislav Tenev <valsinats94@gmail.com>
2018-11-08 13:42:50 +0200

    Add Submit Controller and Submit View

---
+++ MyFirstASP/MyFirstASP/Controllers/SubmitController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstASP.Controllers
+{
+    public class SubmitController : Controller
+    {
+        // GET: Submit
+        public ActionResult Submit()
+        {
+            return View();
+        }
+
+        // GET: Submit/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Submit/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Submit/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Submit));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Submit/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Submit/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Submit));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Submit/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Submit/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Delete));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MyFirstASP/MyFirstASP/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASP.Models
+{
+    public class UserViewModel
+    {
+    }
+}
