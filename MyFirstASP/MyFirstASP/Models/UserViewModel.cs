2d2aba88a6206738d3add52fd216e9996bbea1cd
Stanislav Tenev <valsinats94@gmail.com>
2018-11-08 16:24:09 +0200

    Rearange and Test

--- MyFirstASP/MyFirstASP/Models/UserViewModel.cs
+++ MyFirstASP/MyFirstASP/Models/UserViewModel.cs
@@ -7,5 +7,10 @@ namespace MyFirstASP.Models
 {
     public class UserViewModel
     {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
     }
 }
